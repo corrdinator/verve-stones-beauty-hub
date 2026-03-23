9e37c4543736cbaea0077f591232ddcb58e0c41e
Coordinator Ncube <coordinatormbokodo458600@gmail.com>
2026-03-23 19:25:31 +0200

    Import Menu and X icons from lucide-react

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
